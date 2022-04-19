5d357fb7661bf51183193970c6d59b67b50e588e
lwood54 <lwood3499@gmail.com>
2022-04-18 22:19:04 -0500

    fix typo and change phrase

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -3,7 +3,7 @@ import { Link } from "@remix-run/react";
 export default function Index() {
   return (
     <div className="h-screen flex flex-col items-center justify-center bg-gray-800 text-white gap-2">
-      <h1 className="text-2xl">Welcome to the chate!</h1>
+      <h1 className="text-2xl">Come in and chat!</h1>
       <Link to="/channels">
         Go to the <span className="underline">channels</span> 👉
       </Link>
